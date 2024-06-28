b84aa40a03867ca0aa10363e9aa8e6f7c9eb7ff5
Juan Sosa <sosajseba@gmail.com>
2024-06-28 01:05:19 -0300

    feat: use report service

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using VoltChallenge.Services.Implementations;
+
+Stopwatch stopWatch = new();
+stopWatch.Start();
+
+string? electricityFilePath = Environment.GetEnvironmentVariable("HOUSEHOLD_FILE_PATH");
+string? solarFilePath = Environment.GetEnvironmentVariable("SOLAR_FILE_PATH");
+string? reportFilePath = Environment.GetEnvironmentVariable("REPORT_FILE_PATH");
+
+if (string.IsNullOrEmpty(electricityFilePath) || string.IsNullOrEmpty(solarFilePath) || string.IsNullOrEmpty(reportFilePath))
+{
+    Console.WriteLine("Error: You must configure HOUSEHOLD_FILE_PATH, SOLAR_FILE_PATH and REPORT_FILE_PATH env vars.");
+    return;
+}
+
+ReportService reportService = new();
+
+await reportService.GenerateReportAsync(electricityFilePath, solarFilePath, reportFilePath);
+
+stopWatch.Stop();
+
+TimeSpan ts = stopWatch.Elapsed;
+
+string elapsedTime = string.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+
+Console.WriteLine($"Report generated!");
+Console.WriteLine($"Execution time: {elapsedTime}");
